f8567bd9a99161995912879fe8957d5ad1b91393
Zarpec <axelza25@gmail.com>
2024-10-25 17:43:47 -0600

    Refactor App.js to add login and product list routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./HomeMain";
+import Login from "./login";
+import ProductList from "./listProduct";
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/productList" element={<ProductList />} />
+      </Routes>
+    </Router>
   );
 }
 
