e4796c0f423f809c38772539cab280c9b528257e
Zarpec <axelza25@gmail.com>
2024-10-28 09:38:11 -0600

    Refactor login.js and cargarProd.js
    - Refactor login.js to use React hooks and useEffect for login functionality.
    - Remove unused code and dependencies from cargarProd.js.

--- src/listProduct.js
+++ src/listProduct.js
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from "react";
 import productListT from "../src/View/Product/ProductList.html";
 import { loadProducts } from "./Js/cargarProd";
 import { loadCategory } from "./Js/cargarProd";
+import { loadSubCategory } from "./Js/cargarProd";
 
 const ProductList = () => {
   const [category, setCategory] = useState(null);
   const [subCategory, setSubCategory] = useState(null);
 
-const listCategory = () => {
+  const listCategory = () => {
     loadCategory();
+  };
+
+  const listSubCategory = (category) => {
+    loadSubCategory(category);
   };
 
   useEffect(() => {
@@ -18,6 +23,12 @@ const listCategory = () => {
   useEffect(() => {
     loadProducts(category, subCategory);
   }, [category, subCategory]);
+
+  useEffect(() => {
+    if (category) {
+      listSubCategory(category);
+    }
+  }, [category]);
 
   return (
     <div
