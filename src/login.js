c2227903d8396db55ea2b646b2b7774ec3d178c0
Zarpec <axelza25@gmail.com>
2024-10-28 09:38:21 -0600

    Refactor login.js to use React hooks and add login functionality

--- src/login.js
+++ src/login.js
@@ -1,6 +1,15 @@
+import React, { useEffect, useState } from "react";
 import LoginTemplate from "./View/user/login.html";
-
+import { loginR } from "./Js/login.js";
 const Login = () => {
+  const loginUs = () => {
+    loginR();
+  };
+
+  useEffect(() => {
+    loginUs();
+  }, []);
+
   return <div dangerouslySetInnerHTML={{ __html: LoginTemplate }} />;
 };
 
