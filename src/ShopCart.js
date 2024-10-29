283945f1467125250074c6fd5a507f0084411a5e
Zarpec <axelza25@gmail.com>
2024-10-28 20:39:14 -0600

    Refactor ShopCart.js to use React hooks for session management and improve logout functionality

--- src/ShopCart.js
+++ src/ShopCart.js
@@ -1,7 +1,17 @@
+import React, { useEffect } from "react";
 import ShopCartTem from "./View/Product/ShopCart.html";
-
+import { mostrarCarrito, hiddenProduct } from "./Js/ShopCart";
 
 const ShopCart = () => {
+  useEffect(() => {
+
+    const initCartDisplay = () => {
+      mostrarCarrito();  // Muestra los productos en el carrito
+      hiddenProduct();    // Agrega los productos como campos ocultos
+    };
+    initCartDisplay();
+  }, []);
+
   return <div dangerouslySetInnerHTML={{ __html: ShopCartTem }} />;
 };
 
