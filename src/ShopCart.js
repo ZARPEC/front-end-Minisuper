cb8155b2807d00a55f3f93106691e5ae56a327e1
Zarpec <axelza25@gmail.com>
2024-10-28 18:32:05 -0600

    Add ShopCart component

---
+++ src/ShopCart.js
@@ -0,0 +1,7 @@
+import ShopCartTem from "./View/Product/ShopCart.html";
+
+const ShopCart = () => {
+  return <div dangerouslySetInnerHTML={{ __html: ShopCartTem }} />;
+};
+
+export default ShopCart;
