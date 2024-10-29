b533e15c296066319ca26099b97e6a1a9134fc06
Zarpec <axelza25@gmail.com>
2024-10-28 20:38:59 -0600

    Refactor listProduct.js to add functionality for adding products to the shopping cart

--- src/listProduct.js
+++ src/listProduct.js
@@ -3,6 +3,7 @@ import productListT from "../src/View/Product/ProductList.html";
 import { loadProducts } from "./Js/cargarProd";
 import { loadCategory } from "./Js/cargarProd";
 import { loadSubCategory } from "./Js/cargarProd";
+import { agregarAlCarrito } from "./Js/ShopCart";
 
 const ProductList = () => {
   const [category, setCategory] = useState(null);
@@ -16,19 +17,28 @@ const ProductList = () => {
     loadSubCategory(category);
   };
 
+  const handleAddToCart = (event) => {
+    if (event.target.classList.contains("add-to-cart")) {
+      const productId = event.target.getAttribute("data-id");
+      const productName = event.target.getAttribute("data-nombre");
+      const productPrice = event.target.getAttribute("data-precio");
+
+      agregarAlCarrito({ id: productId, nombre: productName, precio: parseFloat(productPrice), cantidad: 1 });
+    }
+  };
+
   useEffect(() => {
     listCategory();
   }, []);
 
   useEffect(() => {
     loadProducts(category, subCategory);
-  }, [category, subCategory]);
+    // Añade el event listener para los botones "Añadir al carrito"
+    document.querySelector(".Product").addEventListener("click", handleAddToCart);
 
-  useEffect(() => {
-    if (category) {
-      listSubCategory(category);
-    }
-  }, [category]);
+    // Limpia el event listener cuando el componente se desmonte
+    return () => document.querySelector(".Product").removeEventListener("click", handleAddToCart);
+  }, [category, subCategory]);
 
   return (
     <div
